db1aaae028cb8e6f1fb5a67c323c9c4b425e4cd3
sunny <sunnyandroid@sina.com>
2016-05-06 14:29:38 +0800

    MyApplication进行第二次提交做的操作是本地仓库增加了一行注释

--- MyApplication/app/src/main/java/com/example/sunny/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sunny/myapplication/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+/**
+* 在本地的gittest文件夹中添加一行注释。进行的是第二次提交。
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
